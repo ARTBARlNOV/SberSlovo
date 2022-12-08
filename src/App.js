215c83840b043e5b8c420717d7ee14d8043e20f9
ArtBar9981 <scorp9981@gmail.com>
2022-12-09 00:15:50 +0400

    showInfo added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import VerifyBar from './Components/VerifyBar/VerifyBar';
+import showInfo from './Helpers/showInfo';
 
 function App() {
   const [getInfo, setGetInfo] = useState(null);
@@ -12,6 +13,8 @@ function App() {
       <Navbar />
 
       {!getInfo && (<VerifyBar getInfo={getInfo} setGetInfo={setGetInfo} />)}
+
+      {getInfo && showInfo(getInfo)}
     </div>
   );
 }

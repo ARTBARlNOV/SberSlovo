065a0583c13d22517345b950d4ec6dff766231a4
ArtBar9981 <scorp9981@gmail.com>
2022-12-09 00:09:54 +0400

    Navbar,Navbar styles and pictures added

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="Navbar-logo">
+        <img className="Navbar-logo-img" src="/Pictures/Online-Sig.png" alt="Сбер" />
+      </div>
+    </div>
+  );
+}
